e29a1e413a42804fc7307eb1b95f011766913215
Sergey Abakumoff <s.abakumoff@gmail.com>
2014-10-15 21:38:20 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname)); 
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+});
+
+app.post( '/postdata', function( request, response ) {
+	var data = request.body.data
+     return response.send( data );
+});
+
+
+var server = app.listen(82, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+
+});
